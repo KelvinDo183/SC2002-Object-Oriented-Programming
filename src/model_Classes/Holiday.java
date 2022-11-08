fb2976002ed088550a17d91d6f8b7c5fc9b51071
Jia Xun <test@test.com>
2022-11-08 16:24:03 +0800

    added admin config settings (ticket & holiday)

--- src/model_Classes/Holiday.java
+++ src/model_Classes/Holiday.java
@@ -1,21 +1,31 @@
 package model_Classes;
 
+import java.io.Serializable;
 import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
+@SuppressWarnings("serial")
 public class Holiday implements Serializable {
 
-	private LocalDate holidayDate;
+    private LocalDate holidayDate;
 
-	public LocalDate getHolidayDate() {
-		return this.holidayDate;
-	}
+    public Holiday(LocalDate holidayDate){
+        this.holidayDate = holidayDate;
+    }
 
-	/**
-	 * 
-	 * @param holidayDate
-	 */
-	public void setHolidayDate(LocalDate holidayDate) {
-		this.holidayDate = holidayDate;
-	}
+    public LocalDate getHolidayDate(){
+        return this.holidayDate;
+    }
+    
+    public void setHolidayDate(LocalDate holidayDate) {
+        this.holidayDate = holidayDate;
+    }
 
+    public String getHolidayDateToString(){
+        return holidayDate.format(DateTimeFormatter.ofPattern("EEEE, dd/MM/yyyy"));
+    }
+
+    public String toString() {
+        return "The holiday is on " + this.getHolidayDateToString();
+    }
 }
